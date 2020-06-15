87f4a6b97ce2875ebd28e91b1e34bb62fe59f01d
Nathan Reitan <nathanreitan@gmail.com>
2020-06-15 12:40:23 -0700

    charCount and firstDuplicate

--- main.js
+++ main.js
@@ -20,3 +20,13 @@ function filterArray(arr){
   }
   return newArr;
 }
+
+function charCount(myChar, str) {
+  let count = 0
+  for (let i = 0; i<str.length; i++){
+    if (str[i] === myChar){
+      count++
+    }
+  }
+  return count
+}

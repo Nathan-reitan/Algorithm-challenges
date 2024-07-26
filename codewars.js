932d8fd33f81e42ad9618b127df7b4f26fa832f9
Nathan-reitan <nathanreitan@gmail.com>
2024-07-26 11:59:18 -0600

    syntax corrected on old function

--- codewars.js
+++ codewars.js
@@ -177,24 +177,21 @@ const isPrime = (num) => {
 	return true;
 };
 
-const twoSum(numbers, target){
-	return [0,1];
-}
+const twoSum = (numbers, target) => {
+	return [0, 1];
+};
 
-
-const zeros = (n) =>{
-	if (n < 0) // Negative Number Edge Case
-	return -1;
+const zeros = (n) => {
+	if (n < 0)
+		// Negative Number Edge Case
+		return -1;
 
 	// Initialize result
 	let count = 0;
 
 	// Keep dividing n by powers of
 	// 5 and update count
-	for (let i = 5; Math.floor(n / i) >= 1; i *= 5)
-		count += Math.floor(n / i);
+	for (let i = 5; Math.floor(n / i) >= 1; i *= 5) count += Math.floor(n / i);
 
- return count;
-
-
-}
+	return count;
+};
